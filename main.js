ac4efadbaa742f3a8690877a0ee3f3f3f4c1d3ed
Oke Precious Abioye <okepreciousab@gmail.com>
2023-10-20 16:49:04 +0100

    i made correction to navbar

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// alert("Hello World")
+window.onscroll=function(){
+    landingPageScroll()
+}
+const landingPageScroll=()=>{
+    mainNav.style.transition= "3000ms"
+    mainNav.style.background= "transparent"
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop >50){
+        mainNav.style.background= "rgba(0, 0, 0, 0.900)";
+    }
+    else {}
+}
+// const openOffcanvas =()=>{
+    
+// }
